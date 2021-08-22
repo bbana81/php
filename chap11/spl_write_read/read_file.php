e5ca84e76bf5f1794f1d87b8e9d20686e9db04c6
jang-han <jang-han@threet.co.jp>
2021-08-22 23:06:29 +0900

    20210822 chap11 add

--- chap11/spl_write_read/read_file.php
+++ chap11/spl_write_read/read_file.php
@@ -4,35 +4,38 @@ require_once("../../lib/util.php");
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <title>SplFileObjectでファイルを読み込む</title>
     <link href="../../css/style.css" rel="stylesheet">
 </head>
+
 <body>
     <div>
         <?php
         $filename = "mytext.txt";
-        try{
+        try {
             $fileObj = new SplFileObject($filename, "rb");
-        }catch(Exception $e){
+        } catch (Exception $e) {
             echo '<span class = "error">エラーがありました。</span></br>';
             echo $e->getmessage();
             exit();
         }
-        
+
         $readdata = $fileObj->fread($fileObj->getSize());
         if (!($readdata === FALSE)) {
             $readdata = es($readdata);
-            $readdata_br = nl2br($readdata,false);
-            echo "{$filename}を読み込みました。","<br>";
-            echo $readdata_br,"<hr>";
+            $readdata_br = nl2br($readdata, false);
+            echo "{$filename}を読み込みました。", "<br>";
+            echo $readdata_br, "<hr>";
             echo '<a href="write_file.php">ファイルに書き込む</a>';
-        }else{
+        } else {
             echo '<span class="error">ファイルを読み込めませんでした。</span>';
         }
         ?>
-        
+
     </div>
 </body>
+
 </html>
